c16b2c23eb8b4396843e6ef77828ed254f90b915
dhustlerz <jashan.pahwa007@gmail.com>
2015-04-28 17:01:49 -0400

    Dashpage set

---
+++ includes/content.inc.php
@@ -0,0 +1,296 @@
+
+            <div class="page-content">
+                <!-- START X-NAVIGATION VERTICAL -->
+                <ul class="x-navigation x-navigation-horizontal x-navigation-panel page-navigation-top-fixed">
+                    <!-- TOGGLE NAVIGATION -->
+                    <li class="xn-icon-button">
+                        <a href="#" class="x-navigation-minimize"><span class="fa fa-dedent"></span></a>
+                    </li>
+                    <!-- END TOGGLE NAVIGATION -->
+                    <!-- SEARCH -->
+                    <li class="xn-search">
+                        <form role="form">
+                            <input type="text" name="search" placeholder="Search..."/>
+                        </form>
+                    </li>
+                    <!-- END SEARCH -->
+                    <!-- SIGN OUT -->
+                    <li class="xn-icon-button pull-right">
+                        <a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>
+                    </li>
+                    <!-- END SIGN OUT -->
+                    <!-- MESSAGES -->
+                    <!-- <li class="xn-icon-button pull-right">
+                        <a href="#"><span class="fa fa-comments"></span></a>
+                        <div class="informer informer-danger">4</div>
+                        <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><span class="fa fa-comments"></span> Messages</h3>
+                                <div class="pull-right">
+                                    <span class="label label-danger">4 new</span>
+                                </div>
+                            </div>
+                            <div class="panel-body list-group list-group-contacts scroll" style="height: 200px;">
+                                <a href="#" class="list-group-item">
+                                    <div class="list-group-status status-online"></div>
+                                    <img src="assets/images/users/user2.jpg" class="pull-left" alt="John Doe"/>
+                                    <span class="contacts-title">John Doe</span>
+                                    <p>Praesent placerat tellus id augue condimentum</p>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    <div class="list-group-status status-away"></div>
+                                    <img src="assets/images/users/user.jpg" class="pull-left" alt="Dmitry Ivaniuk"/>
+                                    <span class="contacts-title">Dmitry Ivaniuk</span>
+                                    <p>Donec risus sapien, sagittis et magna quis</p>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    <div class="list-group-status status-away"></div>
+                                    <img src="assets/images/users/user3.jpg" class="pull-left" alt="Nadia Ali"/>
+                                    <span class="contacts-title">Nadia Ali</span>
+                                    <p>Mauris vel eros ut nunc rhoncus cursus sed</p>
+                                </a>
+                                <a href="#" class="list-group-item">
+                                    <div class="list-group-status status-offline"></div>
+                                    <img src="assets/images/users/user6.jpg" class="pull-left" alt="Darth Vader"/>
+                                    <span class="contacts-title">Darth Vader</span>
+                                    <p>I want my money back!</p>
+                                </a>
+                            </div>
+                            <div class="panel-footer text-center">
+                                <a href="pages-messages.html">Show all messages</a>
+                            </div>
+                        </div>
+                    </li> -->
+                    <!-- END MESSAGES -->
+                    <!-- TASKS -->
+                   <!--  <li class="xn-icon-button pull-right">
+                        <a href="#"><span class="fa fa-tasks"></span></a>
+                        <div class="informer informer-warning">3</div>
+                        <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><span class="fa fa-tasks"></span> Tasks</h3>
+                                <div class="pull-right">
+                                    <span class="label label-warning">3 active</span>
+                                </div>
+                            </div>
+                            <div class="panel-body list-group scroll" style="height: 200px;">
+                                <a class="list-group-item" href="#">
+                                    <strong>Phasellus augue arcu, elementum</strong>
+                                    <div class="progress progress-small progress-striped active">
+                                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">50%</div>
+                                    </div>
+                                    <small class="text-muted">John Doe, 25 Sep 2014 / 50%</small>
+                                </a>
+                                <a class="list-group-item" href="#">
+                                    <strong>Aenean ac cursus</strong>
+                                    <div class="progress progress-small progress-striped active">
+                                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">80%</div>
+                                    </div>
+                                    <small class="text-muted">Dmitry Ivaniuk, 24 Sep 2014 / 80%</small>
+                                </a>
+                                <a class="list-group-item" href="#">
+                                    <strong>Lorem ipsum dolor</strong>
+                                    <div class="progress progress-small progress-striped active">
+                                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%;">95%</div>
+                                    </div>
+                                    <small class="text-muted">John Doe, 23 Sep 2014 / 95%</small>
+                                </a>
+                                <a class="list-group-item" href="#">
+                                    <strong>Cras suscipit ac quam at tincidunt.</strong>
+                                    <div class="progress progress-small">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
+                                    </div>
+                                    <small class="text-muted">John Doe, 21 Sep 2014 /</small><small class="text-success"> Done</small>
+                                </a>
+                            </div>
+                            <div class="panel-footer text-center">
+                                <a href="pages-tasks.html">Show all tasks</a>
+                            </div>
+                        </div>
+                    </li>-->
+                    <!-- END TASKS -->
+                </ul>
+                <!-- END X-NAVIGATION VERTICAL -->
+
+                <!-- START BREADCRUMB -->
+                <ul class="breadcrumb">
+                    <li><a href="#">Home</a></li>
+                    <li class="active">Dashboard</li>
+                </ul>
+                <!-- END BREADCRUMB -->
+
+                <!-- PAGE CONTENT WRAPPER -->
+                <div class="page-content-wrap text-center margin-top-24 ">
+
+                    <!-- START WIDGETS -->
+                    <div class="row">
+                        <!-- START Total Meetings-->
+                        <div class="col-md-3 ">
+                            <div class="dashpage-square-number-wrapper bg-ffffff">
+                                <div class="dashpage-square-number-top padding-top-10 border-1-e7e7e7">
+                                    <div class="h6 padding-top-6">Total Meetings</div>
+                                    <div class="h3 line-height-1-5">417</div>
+                                </div><div class="row margin-0">
+                                <div class="dashpage-square-number-left col-xs-6 bg-a3bad9 color-002840 padding-0 height-52">
+                                    <div class="margin-top-6 margin-bottom-6">387</div>
+                                    <div class="font-size-10">Accredited</div>
+                                </div>
+                                <div class="dashpage-square-number-right col-xs-6 bg-005180 color-fff padding-0 height-52">
+                                    <div class="margin-top-6 margin-bottom-6">30</div>
+                                    <div class="font-size-10 text-center">Non Accred</div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END Total Meetings -->
+                        </div>
+                        <div class="col-md-3 text-center">
+
+                            <!-- START WIDGET MESSAGES -->
+
+                                <div class="dashpage-square-number-wrapper  bg-ffffff">
+                                    <div class="dashpage-square-number-top padding-top-10 border-1-e7e7e7">
+                                        <div class="h6 padding-top-6">Evaluations Received</div>
+                                        <div class="h3 line-height-1-5">2169</div>
+                                    </div><div class="row margin-0">
+                                    <div class="dashpage-square-number-left col-xs-6 bg-a3bad9 color-002840 padding-0 height-52">
+                                        <div class="margin-top-6 margin-bottom-6">1937</div>
+                                        <div class="font-size-10">Accredited</div>
+                                    </div>
+                                    <div class="dashpage-square-number-right col-xs-6 bg-005180 color-fff padding-0 height-52">
+                                        <div class="margin-top-6 margin-bottom-6">232</div>
+                                        <div class="font-size-10 text-center">Non Accred</div>
+                                    </div>
+                                </div>
+                        </div>
+
+                            <!-- END WIDGET MESSAGES -->
+
+                        </div>
+                        <div class="col-md-3 text-center" >
+
+                            <!-- START WIDGET REGISTRED -->
+                            <div class="dashpage-square-number-wrapper  bg-ffffff">
+                                    <div class="dashpage-square-number-top padding-top-10 border-1-e7e7e7">
+                                        <div class="h6 padding-top-6">Avg. Program NTS</div>
+                                        <div class="h3 line-height-1-5">+14</div>
+                                    </div><div class="row margin-0">
+                                    <div class="dashpage-square-number-left col-xs-6 bg-a3bad9 color-002840 padding-0 height-52">
+                                        <div class="margin-top-6 margin-bottom-6">+16</div>
+                                        <div class="font-size-10">Accredited</div>
+                                    </div>
+                                    <div class="dashpage-square-number-right col-xs-6 bg-005180 color-fff padding-0 height-52">
+                                        <div class="margin-top-6 margin-bottom-6">-5</div>
+                                        <div class="font-size-10 text-center">Non Accred</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END WIDGET REGISTRED -->
+
+                        </div>
+                        <div class="col-md-3 text-center">
+
+                            <!-- START WIDGET CLOCK -->
+                             <div class="dashpage-square-number-wrapper  bg-ffffff">
+                                    <div class="dashpage-square-number-top padding-top-10 border-1-e7e7e7">
+                                        <div class="h6 padding-top-6">Avg. Speaker Rating</div>
+                                        <div class="h3 line-height-1-5">4.64</div>
+                                    </div><div class="row margin-0">
+                                    <div class="dashpage-square-number-left col-xs-6 bg-a3bad9 color-002840 padding-0 height-52">
+                                        <div class="margin-top-6 margin-bottom-6">4.66</div>
+                                        <div class="font-size-10">Accredited</div>
+                                    </div>
+                                    <div class="dashpage-square-number-right col-xs-6 bg-005180 color-fff padding-0 height-52">
+                                        <div class="margin-top-6 margin-bottom-6">4.50</div>
+                                        <div class="font-size-10 text-center">Non Accred</div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <!-- END WIDGET CLOCK -->
+
+                        </div>
+
+                    </div>
+                    <!-- END WIDGETS -->
+
+                    <div class="row margin-top-24" >
+                        <div class="col-md-6">
+                            <!-- START STACKED CHART -->
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div id="chart-5" style="height: 300px;"><svg></svg></div>
+                                </div>
+                            </div>
+                            <!-- END STACKED CHART -->
+                        </div>
+
+                         <div class="col-md-6">
+                            <!-- START REGULAR PIE CHART -->
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div id="chart-9" style="height: 300px;"><svg></svg></div>
+                                </div>
+                            </div>
+                            <!-- END REGULAR PIE CHART -->
+                        </div>
+
+
+                    </div>
+
+                    <div class="row">
+                          <div class="col-md-6">
+                            <!-- START STACKED CHART -->
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div id="chart-5" style="height: 300px;"><svg></svg></div>
+                                </div>
+                            </div>
+                            <!-- END STACKED CHART -->
+                        </div>
+                          <div class="col-md-6">
+                            <!-- START STACKED CHART -->
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div id="chart-5" style="height: 300px;"><svg></svg></div>
+                                </div>
+                            </div>
+                            <!-- END STACKED CHART -->
+                        </div>
+                    </div>
+
+                    <div class="row">
+                         <!-- START GOOGLE WORLD MAP -->
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-body panel-body-map">
+                                    <div id="google_world_map" style="height: 300px;"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END GOOGLE WORLD MAP -->
+
+                      <div class="row">
+                          <div class="col-md-6">
+                            <!-- START DatatTables -->
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div id="chart-5" style="height: 300px;"><svg></svg></div>
+                                </div>
+                            </div>
+                            <!-- END DatatTables-->
+                        </div>
+                          <div class="col-md-6">
+                            <!-- START DatatTables-->
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div id="chart-5" style="height: 300px;"><svg></svg></div>
+                                </div>
+                            </div>
+                            <!-- END DatatTables -->
+                        </div>
+                    </div>
+                </div>
+                <!-- END PAGE CONTENT WRAPPER -->
+            </div>
