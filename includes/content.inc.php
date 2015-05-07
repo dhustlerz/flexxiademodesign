09d6dca7023a552584843a3317cbdbe5579b7e2d
dhustlerz <jashan.pahwa007@gmail.com>
2015-05-07 17:04:50 -0400

    Update Dashboard

--- includes/content.inc.php
+++ includes/content.inc.php
@@ -82,11 +82,11 @@ $output.='<div class="page-content-wrap ">';
            /*--------------------------------------------------------------------------------------*/
             $output.='<div class="dashpage-square-number-wrapper">';
                 $output.='<div class="dashpage-square-number-top">';
-                  $output.='<div class="col-md-12 bg-ffffff">';
+                  $output.='<div class="row bg-ffffff">';
 
                     $output.='<div class="col-md-6 col-xs-6 " style="">';
                     /*--------------------------------------------------------------------------------------*/
-                      $output.='<div class=" padding-top-10 float-right"><img src="img/icons/cal-icon.png" alt="Mountain View" style="height:50px">';
+                      $output.='<div class=" padding-top-10 float-right"><img src="img/icons/cald-icon.png" alt="Mountain View" style="height:50px">';
                       $output.='</div>';
                     /*--------------------------------------------------------------------------------------*/
                     $output.='</div>';
@@ -102,27 +102,30 @@ $output.='<div class="page-content-wrap ">';
                 $output.='</div>';
             $output.='</div>';
            /*--------------------------------------------------------------------------------------*/
-                $output.='<div class="col-md-12  bg-a3bad9 margin-top-10">';
-                    $output.='<div class="dashpage-square-number-left  padding-0 height-52">';
+                $output.='<div class="row  bg-2DAAE1 margin-top-12">';
+                  $output.='<div class=" col-md-12 padding-top-12">';
+                  /*--------------------------------------------------------------------------------------*/
+                    $output.='<div class="col-md-1 col-xs-1 "><span class="font-size-18 color-fff col-centered">4.52</span></div>';
 
-                      $output.='<div class="xe-progress">';
-                        //$output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56" data-fill-unit="%" data-fill-property="width" data-fill-duration="2" data-fill-easing="true" style="width:56%"></span>';
-                        $output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56"></span>';
-
+                    $output.='<div class=" col-md-11 col-xs-11">';
+                    /*--------------------------------------------------------------------------------------*/
+                      $output.='<div class="dashpage-square-number-left height-26 ">';
+                        $output.='<div class="xe-progress ">';
+                          $output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56" data-fill-unit="" data-fill-property="width" data-fill-duration="2" data-fill-easing="true" style="width:100px;;"></span>';
+                          $output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56"></span>';
+                        $output.='</div>';
                       $output.='</div>';
-                      // $output.='<div class="progress">';
-                      //   $output.='<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">';
-                      //     $output.='<span class="sr-only">60% Complete</span>';
-                      //   $output.='</div>';
-                      // $output.='</div>';
+                        // $output.='<div class="margin-top-6 margin-bottom-6"></div>';
+                        // $output.='<div class="font-size-10"></div>';
+                    $output.='</div>';
+                    /*--------------------------------------------------------------------------------------*/
+                  $output.='</div>';
+                  /*--------------------------------------------------------------------------------------*/
 
-                        $output.='<div class="margin-top-6 margin-bottom-6"></div>';
-                        $output.='<div class="font-size-10"></div>';
-                    $output.='</div>';
-                    $output.='<div class="dashpage-square-number-right col-xs-6   padding-0 height-52">';
-                        $output.='<div class="margin-top-6 margin-bottom-6"></div>';
-                        $output.='<div class="font-size-10 text-center"></div>';
-                    $output.='</div>';
+                  $output.='<div class="col-md-12">';
+                      $output.='<div class="padding-left-10 font-size-16 color-fff margin-bottom-8">Average Speaker Rating</div>';
+                      //$output.='<div class="font-size-10 text-center"></div>';
+                  $output.='</div>';
                 $output.='</div>';
         $output.='</div>';
         /**
@@ -131,78 +134,109 @@ $output.='<div class="page-content-wrap ">';
         $output.='</div>';
         $output.='<div class="col-md-4 ">';
 
-            /**
-             * START WIDGET MESSAGES
-             */
-
-                 $output.='<div class="dashpage-square-number-wrapper bg-ffffff">';
-                $output.='<div class="dashpage-square-number-top padding-top-10 ">';
-                $output.='<div class=" col-xs-12 ">';
+        /*--------------------------------------------------------------------------------------*/
+          $output.='<div class="dashpage-square-number-wrapper">';
+              $output.='<div class="dashpage-square-number-top">';
+                $output.='<div class="row bg-ffffff">';
 
                   $output.='<div class="col-md-6 col-xs-6 " style="">';
+                  /*--------------------------------------------------------------------------------------*/
                     $output.='<div class=" padding-top-10 float-right"><img src="img/icons/linegraph-icon.png" alt="Mountain View" style="height:50px">';
                     $output.='</div>';
+                  /*--------------------------------------------------------------------------------------*/
                   $output.='</div>';
-                    $output.='<div class="col-md-6">';
-                      $output.='<div class="dashpage-square-number-top padding-top-10 padding-bottom-10">';
-                        $output.='<div class="animate-number-1 h3 line-height-1-5">1,275</div>';
-                        $output.='<div class="h6 padding-top-6">Total Evaluations</div>';
+
+                  $output.='<div class="col-md-6">';
+                  /*--------------------------------------------------------------------------------------*/
+                    $output.='<div class="dashpage-square-number-top padding-top-10 padding-bottom-10">';
+                      $output.='<div class="animate-number-1 h3 line-height-1-5">1,275</div>';
+                      $output.='<div class="h6 padding-top-6">Total Evaluations</div>';
+                    $output.='</div>';
+                  /*--------------------------------------------------------------------------------------*/
+                $output.='</div>';
+              $output.='</div>';
+          $output.='</div>';
+         /*--------------------------------------------------------------------------------------*/
+              $output.='<div class="row  bg-3C5B9B margin-top-12">';
+                $output.='<div class=" col-md-12 padding-top-12">';
+                /*--------------------------------------------------------------------------------------*/
+                  $output.='<div class="col-md-1 col-xs-1 "><span class="font-size-18 color-fff col-centered">4.25</span></div>';
+
+                  $output.='<div class=" col-md-11 col-xs-11">';
+                  /*--------------------------------------------------------------------------------------*/
+                    $output.='<div class="dashpage-square-number-left height-26 ">';
+                      $output.='<div class="xe-progress ">';
+                        $output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56" data-fill-unit="" data-fill-property="width" data-fill-duration="2" data-fill-easing="true" style="width:200px;;"></span>';
+                        $output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56"></span>';
                       $output.='</div>';
                     $output.='</div>';
+                      // $output.='<div class="margin-top-6 margin-bottom-6"></div>';
+                      // $output.='<div class="font-size-10"></div>';
                   $output.='</div>';
+                  /*--------------------------------------------------------------------------------------*/
+                $output.='</div>';
+                /*--------------------------------------------------------------------------------------*/
 
-                $output.='</div><div class="row margin-top-10">';
-                // $output.='<div class="dashpage-square-number-left col-xs-6 bg-a3bad9 color-002840 padding-0 height-52">';
-                //     $output.='<div class="margin-top-6 margin-bottom-6">387</div>';
-                //     $output.='<div class="font-size-10">Accredited</div>';
-                // $output.='</div>';
-                // $output.='<div class="dashpage-square-number-right col-xs-6 bg-005180 color-fff padding-0 height-52">';
-                //     $output.='<div class="margin-top-6 margin-bottom-6">30</div>';
-                //     $output.='<div class="font-size-10 text-center">Non Accred</div>';
-                // $output.='</div>';
-            $output.='</div>';
+                $output.='<div class="col-md-12">';
+                    $output.='<div class="padding-left-10 font-size-16 color-fff margin-bottom-8">Average Program Rating</div>';
+                    //$output.='<div class="font-size-10 text-center"></div>';
+                $output.='</div>';
+              $output.='</div>';
+
         $output.='</div>';
-
-            /**
-             * END WIDGET MESSAGES
-             */
 
         $output.='</div>';
         $output.='<div class="col-md-4" >';
+                     /*--------------------------------------------------------------------------------------*/
+            $output.='<div class="dashpage-square-number-wrapper">';
+                $output.='<div class="dashpage-square-number-top">';
+                  $output.='<div class="row bg-ffffff">';
 
-            /**
-             * START WIDGET REGISTRED
-             */
-             $output.='<div class="dashpage-square-number-wrapper bg-ffffff">';
-                $output.='<div class="dashpage-square-number-top padding-top-10 ">';
-                $output.='<div class=" col-xs-12 ">';
-
-                  $output.='<div class="col-md-6 col-xs-6 " style="">';
-                    $output.='<div class=" padding-top-10 float-right"><img src="img/icons/user-icon.png" alt="Mountain View" style="height:50px">';
+                    $output.='<div class="col-md-6 col-xs-6 " style="">';
+                    /*--------------------------------------------------------------------------------------*/
+                      $output.='<div class=" padding-top-10 float-right"><img src="img/icons/user-icon.png" alt="Mountain View" style="height:50px">';
+                      $output.='</div>';
+                    /*--------------------------------------------------------------------------------------*/
                     $output.='</div>';
-                  $output.='</div>';
+
                     $output.='<div class="col-md-6">';
+                    /*--------------------------------------------------------------------------------------*/
                       $output.='<div class="dashpage-square-number-top padding-top-10 padding-bottom-10">';
-                        $output.='<div class="animate-number-2 h3 line-height-1-5">1,153</div>';
+                        $output.='<div class="animate-number h3 line-height-1-5">1,530</div>';
                         $output.='<div class="h6 padding-top-6">Total Attendees</div>';
                       $output.='</div>';
-                    $output.='</div>';
+                    /*--------------------------------------------------------------------------------------*/
                   $output.='</div>';
-
-                $output.='</div><div class="row margin-top-10">';
-                // $output.='<div class="dashpage-square-number-left col-xs-6 bg-a3bad9 color-002840 padding-0 height-52">';
-                //     $output.='<div class="margin-top-6 margin-bottom-6">387</div>';
-                //     $output.='<div class="font-size-10">Accredited</div>';
-                // $output.='</div>';
-                // $output.='<div class="dashpage-square-number-right col-xs-6 bg-005180 color-fff padding-0 height-52">';
-                //     $output.='<div class="margin-top-6 margin-bottom-6">30</div>';
-                //     $output.='<div class="font-size-10 text-center">Non Accred</div>';
-                // $output.='</div>';
+                $output.='</div>';
             $output.='</div>';
+           /*--------------------------------------------------------------------------------------*/
+                $output.='<div class="row  bg-0173B2 margin-top-12">';
+                  $output.='<div class=" col-md-12 padding-top-12">';
+                  /*--------------------------------------------------------------------------------------*/
+                    $output.='<div class="col-md-1 col-xs-1 "><span class="font-size-18 color-fff col-centered">88.3</span></div>';
+
+                    $output.='<div class=" col-md-11 col-xs-11">';
+                    /*--------------------------------------------------------------------------------------*/
+                      $output.='<div class="dashpage-square-number-left height-26 ">';
+                        $output.='<div class="xe-progress ">';
+                          $output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56" data-fill-unit="" data-fill-property="width" data-fill-duration="2" data-fill-easing="true" style="width:150px;;"></span>';
+                          $output.='<span class="xe-progress-fill" data-fill-from="0" data-fill-to="56"></span>';
+                        $output.='</div>';
+                      $output.='</div>';
+                        // $output.='<div class="margin-top-6 margin-bottom-6"></div>';
+                        // $output.='<div class="font-size-10"></div>';
+                    $output.='</div>';
+                    /*--------------------------------------------------------------------------------------*/
+                  $output.='</div>';
+                  /*--------------------------------------------------------------------------------------*/
+
+                  $output.='<div class="col-md-12">';
+                      $output.='<div class="padding-left-10 font-size-16 color-fff margin-bottom-8">Customer Satisfaction</div>';
+                      //$output.='<div class="font-size-10 text-center"></div>';
+                  $output.='</div>';
+                $output.='</div>';
         $output.='</div>';
-            /**
-             * END WIDGET REGISTRED
-             */
+
 
         $output.='</div>';
     //     $output.='<div class="col-md-3 text-center">';
