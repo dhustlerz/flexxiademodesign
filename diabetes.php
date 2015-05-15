444ae3eca69118163ac2e8a21bcd75a4f39fdea2
dhustlerz <jashan.pahwa007@gmail.com>
2015-05-15 19:23:39 -0400

    update Diabetes

---
+++ diabetes.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'includes/head.inc.php' ?>
+    </head>
+    <body>
+        <?php include 'includes/diabetes/body-diabetes.inc.php'; ?>
+
+    </body>
+</html>
