aa2cf28878ee3d21638d74426a6f9b0cb7219d29
dhustlerz <jashan.pahwa007@gmail.com>
2015-06-02 12:14:50 -0400

    Clean

--- js/demo_gauge.js
+++ js/demo_gauge.js
@@ -1,75 +1,50 @@
+  /* gauge.js start */
+if (window.location.pathname+window.location.search == '/flexxiademodesign/dashboard.php') {
+  console.log('working');
+  gaugejs("dashboard-gauge","dashboard-gauge-font",90);
+}
+if (window.location.pathname+window.location.search == '/flexxiademodesign/diabetes.php') {
+  gaugejs("diabetes-gauge","diabetes-gauge-font",90);
+}
+else if (window.location.pathname+window.location.search == '/flexxiademodesign/diabetes.php?s=acceredited') {
+  gaugejs("diabetes-gauge-accr","diabetes-gauge-accr-value",95);
+}
+else if (window.location.pathname+window.location.search == '/flexxiademodesign/diabetes.php?s=non-acceredited') {
+  gaugejs("diabetes-gauge-non-accr","diabetes-gauge-non-accr-value",85);
+}
 
-          /* gauge.js start */
-        //gaugejs("dashboard-gauge","dashboard-gauge-font",90);
-        //gaugejs("diabetes-gauge","diabetes-gauge-font",90);
-        //gaugejs("diabetes-gauge-accr","diabetes-gauge-accr-value",95);
-        //gaugejs("diabetes-non-accr-gauge","diabetes-non-accr-gauge-font",85);
+function gaugejs(GaugeId,GaugeTextId,GaugeValue) {
+
+  var opts = {
+      lines: 12, // The number of lines to draw
+      angle: 0, // The length of each line
+      lineWidth: 0.20, // The line thickness
+      pointer: {
+        length: 0.78, // The radius of the inner circle
+        strokeWidth: 0.015, // The rotation offset
+        color: '#000000' // Fill color
+      },
+      limitMax: 'false',   // If true, the pointer will not go past the end of the gauge
+      colorStart: '#2DAAE1',   // Colors
+      colorStop: '#2DAAE1',    // just experiment with them
+      strokeColor: '#E0E0E0',   // to see which ones work best for you
+      generateGradient: true
+    };
+    var target = document.getElementById(GaugeId); // your canvas element
+    var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+    gauge.setTextField(document.getElementById(GaugeTextId));
+    // document.getElementById('preview-textfield-gauge').innerHTML = "%";
+    var textRenderer = new TextRenderer(document.getElementById(GaugeTextId))
+    textRenderer.render = function(gauge) {
+           percentage = gauge.displayedValue / gauge.maxValue
+           this.el.innerHTML = (percentage * 100).toFixed(0) + "%"
+        };
+    gauge.setTextField(textRenderer);
+    gauge.maxValue = 100; // set max gauge value
+    gauge.animationSpeed = 6; // set animation speed (32 is default value)
+    gauge.set(GaugeValue); // set actual value
+}
 
 
-          var opts = {
-              lines: 12, // The number of lines to draw
-              angle: 0, // The length of each line
-              lineWidth: 0.20, // The line thickness
-              pointer: {
-                length: 0.78, // The radius of the inner circle
-                strokeWidth: 0.015, // The rotation offset
-                color: '#000000' // Fill color
-              },
-              limitMax: 'false',   // If true, the pointer will not go past the end of the gauge
-              colorStart: '#2DAAE1',   // Colors
-              colorStop: '#2DAAE1',    // just experiment with them
-              strokeColor: '#E0E0E0',   // to see which ones work best for you
-              generateGradient: true
-            };
-             if (window.location.pathname+window.location.search == '/flexxia-demo/diabetes.php') {
-            var targetDiabetes = document.getElementById("diabetes-gauge"); // your canvas element
-            var gaugeDiabetes = new Gauge(targetDiabetes).setOptions(opts); // create sexy gauge!
-            gaugeDiabetes.setTextField(document.getElementById("diabetes-gauge-font"));
-            // document.getElementById('preview-textfield-gauge').innerHTML = "%";
-            var textRenderer = new TextRenderer(document.getElementById("diabetes-gauge-font"));
-            textRenderer.render = function(gaugeDiabetes) {
-                   percentage = gaugeDiabetes.displayedValue / gaugeDiabetes.maxValue
-                   this.el.innerHTML = (percentage * 100).toFixed(0) + "%"
-                };
-            gaugeDiabetes.setTextField(textRenderer);
-            gaugeDiabetes.maxValue = 100; // set max gauge value
-            gaugeDiabetes.animationSpeed = 6; // set animation speed (32 is default value)
-            gaugeDiabetes.set(90); // set actual value
-          }
-          else if (window.location.pathname+window.location.search == '/flexxia-demo/diabetes.php?s=acceredited') {
-            /*Gauge for diabetes accreted program*/
-             var targetDiabetesAccr = document.getElementById("diabetes-gauge-accr"); // your canvas element
-            var gaugeDiabetesAccr = new Gauge(targetDiabetesAccr).setOptions(opts); // create sexy gauge!
-            gaugeDiabetesAccr.setTextField(document.getElementById("diabetes-gauge-accr-value"));
-            // document.getElementById('preview-textfield-gauge').innerHTML = "%";
-            var textRenderer = new TextRenderer(document.getElementById("diabetes-gauge-accr-value"));
-            textRenderer.render = function(gaugeDiabetesAccr) {
-                   percentage = gaugeDiabetesAccr.displayedValue / gaugeDiabetesAccr.maxValue
-                   this.el.innerHTML = (percentage * 100).toFixed(0) + "%"
-                };
-            gaugeDiabetesAccr.setTextField(textRenderer);
-            gaugeDiabetesAccr.maxValue = 100; // set max gauge value
-            gaugeDiabetesAccr.animationSpeed = 6; // set animation speed (32 is default value)
-            gaugeDiabetesAccr.set(95); // set actual value
 
-          }
-           else if (window.location.pathname+window.location.search == '/flexxia-demo/diabetes.php?s=non-acceredited') {
-            /*Gauge for diabetes accreted program*/
-             var targetDiabetesAccr = document.getElementById("diabetes-gauge-non-accr"); // your canvas element
-            var gaugeDiabetesAccr = new Gauge(targetDiabetesAccr).setOptions(opts); // create sexy gauge!
-            gaugeDiabetesAccr.setTextField(document.getElementById("diabetes-gauge-non-accr-value"));
-            // document.getElementById('preview-textfield-gauge').innerHTML = "%";
-            var textRenderer = new TextRenderer(document.getElementById("diabetes-gauge-non-accr-value"));
-            textRenderer.render = function(gaugeDiabetesAccr) {
-                   percentage = gaugeDiabetesAccr.displayedValue / gaugeDiabetesAccr.maxValue
-                   this.el.innerHTML = (percentage * 100).toFixed(0) + "%"
-                };
-            gaugeDiabetesAccr.setTextField(textRenderer);
-            gaugeDiabetesAccr.maxValue = 100; // set max gauge value
-            gaugeDiabetesAccr.animationSpeed = 6; // set animation speed (32 is default value)
-            gaugeDiabetesAccr.set(85); // set actual value
-
-          }
-
-            /* gauge.js end*/
 
