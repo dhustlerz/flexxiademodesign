b0d4714426833b47b75fa3a18087b5d707090dd5
dhustlerz <jashan.pahwa007@gmail.com>
2015-07-09 15:44:33 -0400

    error fixed

--- az.php
+++ az.php
@@ -19,12 +19,12 @@
 
     <div style="width:911px;" class=" container">
 
-      <div  style="background-image:url(img/az/azbg2.png); height: 675px; width: 911px; border: 1px solid #e5edf2; ">
+      <div  style="background-image:url(img/az/azmain2.png); height: 675px; width: 911px; border: 1px solid #e5edf2; ">
          <div class="row">
           <div class="col-sm-1">
           <a   onClick="noty({text: 'Meeting has been successfully created', layout: 'topRight', type: 'success'});"><img style=" cursor: pointer; height: 23px; width: 25px; margin-left: 540px; margin-top: 9px;" src="img/az/oneclick.png"> </a>
         </div>
-          <a   onClick="noty({text: 'Meeting has been successfully created', layout: 'topRight', type: 'success'});"><img style=" cursor: pointer; height: 23px;  margin-left: 720px; margin-top: 114px;" src="img/az/drop-down-one-click.png"> </a>
+          <a   onClick="noty({text: 'Meeting has been successfully created', layout: 'topRight', type: 'success'});"><img style=" cursor: pointer; height: 23px;  margin-left: 720px; margin-top: 114px;" src="img/az/wx-drop-down-one-click.png"> </a>
 
 
 
