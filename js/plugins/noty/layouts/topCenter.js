94fe674d5db0a217ee69621d10f81c3d99f9b0b4
dhustlerz <jashan.pahwa007@gmail.com>
2015-07-10 13:27:45 -0400

    clean code

--- js/plugins/noty/layouts/topCenter.js
+++ js/plugins/noty/layouts/topCenter.js
@@ -1,41 +1,41 @@
 (function(jQuery) {
 
-    jQuery.noty.layouts.topCenter = {
-        name     : 'topCenter',
-        options  : { // overrides options
+  jQuery.noty.layouts.topCenter = {
+    name     : 'topCenter',
+    options  : { // overrides options
 
-        },
-        container: {
-            object  : '<ul id="noty_topCenter_layout_container" />',
-            selector: 'ul#noty_topCenter_layout_container',
-            style   : function() {
-                jQuery(this).css({
-                    top          : 40,
-                    left         : 0,
-                    position     : 'fixed',
-                    width        : '310px',
-                    height       : 'auto',
-                    margin       : 0,
-                    padding      : 0,
-                    listStyleType: 'none',
-                    zIndex       : 10000000
-                });
+    },
+    container: {
+      object  : '<ul id="noty_topCenter_layout_container" />',
+      selector: 'ul#noty_topCenter_layout_container',
+      style   : function() {
+        jQuery(this).css({
+          top          : 40,
+          left         : 0,
+          position     : 'fixed',
+          width        : '310px',
+          height       : 'auto',
+          margin       : 0,
+          padding      : 0,
+          listStyleType: 'none',
+          zIndex       : 10000000
+        });
 
-                jQuery(this).css({
-                    left: (jQuery(window).width() - jQuery(this).outerWidth(false)) / 2 + 'px'
-                });
-            }
-        },
-        parent   : {
-            object  : '<li />',
-            selector: 'li',
-            css     : {}
-        },
-        css      : {
-            display: 'none',
-            width  : '310px'
-        },
-        addClass : ''
-    };
+        jQuery(this).css({
+            left: (jQuery(window).width() - jQuery(this).outerWidth(false)) / 2 + 'px'
+        });
+      }
+    },
+    parent   : {
+      object  : '<li />',
+      selector: 'li',
+      css     : {}
+    },
+    css      : {
+      display: 'none',
+      width  : '310px'
+    },
+    addClass : ''
+  };
 
 })(jQuery);
