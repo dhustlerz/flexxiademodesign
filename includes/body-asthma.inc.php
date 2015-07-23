1f5f42ca1e9e40df3888811c0071e888e59e71a1
dhustlerz <jashan.pahwa007@gmail.com>
2015-07-23 15:38:11 -0400

    clean code

--- includes/body-asthma.inc.php
+++ includes/body-asthma.inc.php
@@ -1,32 +1,32 @@
 <!-- START PAGE CONTAINER -->
-        <div  class="page-container">
+<div  class="page-container">
 
-            <?php
-             // START PAGE SIDEBAR
-             include 'includes/sidebar-asthma.inc.php' ;
-             // END PAGE SIDEBAR -->
+    <?php
+     // START PAGE SIDEBAR
+     include 'includes/sidebar-asthma.inc.php' ;
+     // END PAGE SIDEBAR -->
 
-             // PAGE CONTENT
-            if ( isset($_GET['s']) && $_GET['s'] == 'acceredited') {
-                   include 'includes/content-asthma-accredited.inc.php';
-            }
-            elseif (isset($_GET['s']) && $_GET['s'] == 'non-acceredited') {
-                include 'includes/content-asthma-non-accredited.inc.php';
-            }
-            else {
-                     include 'includes/content-asthma.inc.php';
-                }
+     // PAGE CONTENT
+    if ( isset($_GET['s']) && $_GET['s'] == 'acceredited') {
+           include 'includes/content-asthma-accredited.inc.php';
+    }
+    elseif (isset($_GET['s']) && $_GET['s'] == 'non-acceredited') {
+        include 'includes/content-asthma-non-accredited.inc.php';
+    }
+    else {
+             include 'includes/content-asthma.inc.php';
+        }
 
-             // END PAGE CONTENT -->
-            ?>
+     // END PAGE CONTENT -->
+    ?>
 
 
 
-        </div>
-        <!-- END PAGE CONTAINER -->
+</div>
+<!-- END PAGE CONTAINER -->
 
-        <?php
+<?php
 
-         include 'javascript.inc.php';
+ include 'javascript.inc.php';
 
-        ?>
+?>
